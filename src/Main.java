979de12bdc080dede98d6506d19a7ef32e1ee192
Azuras03 <nr03russo@gmail.com>
2023-12-11 09:26:46 +0100

    Ajout de données primaires pour la classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,38 @@
+import lib.MLP;
+import lib.Sigmoide;
+import lib.TransferFunction;
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         //On stocke le nombre de neurones par couche
+        int[] layers = {2, 3, 1};
         //On stocke le learning rate
+        double learningRate = 0.1;
         //On stocke la fonction de transfert
+        TransferFunction transferFunction = new Sigmoide();
         //On stocke le nombre d'époques
+        int epochs = 10000;
 
         //On crée le MLP
+        MLP mlp = new MLP(layers, learningRate, transferFunction);
 
+        double[] output = {0};
+        // table ET
+        double[] input = {0,0};
         //Les types des sorties sont soit des doubles soit des tableaux de doubles
-
+        int i = 0;
         //Boucle infinie
+        while (i < epochs) {
             //On calcule la sortie du MLP
+            output = mlp.execute(input);
             //On calcule l'erreur
-        //Fin boucle infinie
+            double error = output[0] - 0;
+            //Fin boucle infinie
+            i++;
+        }
     }
 
 }
