0b6551c0fec42a644aff6110599d6e81aca51964
Thomas <robineau1u@etu.univ-lorraine.fr>
2023-12-11 08:44:51 +0100

    Add pseudo code

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //On stocke le nombre de neurones par couche
+        //On stocke le learning rate
+        //On stocke la fonction de transfert
+        //On stocke le nombre d'époques
+
+        //On crée le MLP
+
+        //Les types des sorties sont soit des doubles soit des tableaux de doubles
+
+        //Boucle infinie
+            //On calcule la sortie du MLP
+            //On calcule l'erreur
+        //Fin boucle infinie
+    }
+
+}
