f8ef12ee70e23b3fcc6704d8b8df46a8461c0c8c
Azuras03 <nr03russo@gmail.com>
2023-12-18 09:14:07 +0100

    Mise à jour des classes AStar et DFS

--- src/rushhour/ia/algo/recherche/AStar.java
+++ src/rushhour/ia/algo/recherche/AStar.java
@@ -1,56 +1,71 @@
 package rushhour.ia.algo.recherche;
 
 import rushhour.ia.framework.common.Action;
+import rushhour.ia.framework.common.ArgParse;
 import rushhour.ia.framework.common.State;
 import rushhour.ia.framework.recherche.HasHeuristic;
 import rushhour.ia.framework.recherche.SearchNode;
 import rushhour.ia.framework.recherche.SearchProblem;
 import rushhour.ia.framework.recherche.TreeSearch;
 
-public class AStar extends TreeSearch implements HasHeuristic {
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.LinkedList;
 
-    /**
-     * Crée un algorithme de recherche
-     *
-     * @param p Le problème à résoudre
-     * @param s L'état initial
-     */
+public class AStar extends TreeSearch implements HasHeuristic {
     public AStar(SearchProblem p, State s) {
         super(p, s);
     }
 
     @Override
-    public double getHeuristic() {
-        return 0;
-    }
-
-    @Override
     public boolean solve() {
-        SearchNode node = SearchNode.makeRootSearchNode(intial_state);
-        frontier.add(node);
-        explored.add(node.getState());
-
+        end_node = SearchNode.makeRootSearchNode(intial_state);
+        intial_state = end_node.getState();
+        frontier = new LinkedList<>();
+        frontier.add(end_node);
+        explored = new HashSet<>();
+        if (ArgParse.DEBUG) {
+            System.out.print("[" + intial_state);
+        }
         while (!frontier.isEmpty()) {
-            node = frontier.poll();
-            if (problem.isGoalState(node.getState())) {
-                end_node = node;
-                return true;
+            end_node = frontier.remove();
+            intial_state = end_node.getState();
+            if (ArgParse.DEBUG) {
+                System.out.print(" + " + end_node.getAction() + "] -> [" + intial_state);
             }
-            explored.add(node.getState());
-            for (Action a : problem.getActions(node.getState())) {
-                SearchNode s = SearchNode.makeChildSearchNode(this.problem, node, a);
-                if (!explored.contains(s.getState()) && !frontier.contains(s)) {
-                    frontier.add(s);
-                } else if (frontier.contains(s)) {
+            if (problem.isGoalState(intial_state)) {
+                if (ArgParse.DEBUG) {
+                    System.out.println("]");
+                }
+                return true;
+            } else {
+                explored.add(intial_state);
+            }
+            for (Action a : problem.getActions(intial_state)) {
+                SearchNode child = SearchNode.makeChildSearchNode(problem, end_node, a);
+                State child_state = child.getState();
+                if (!frontier.contains(child) && !explored.contains(child_state)) {
+                    frontier.add(child);
+                } else if (frontier.contains(child)) {
                     for (SearchNode n : frontier) {
-                        if (n.equals(s) && n.getHeuristic() + n.getCost() > s.getHeuristic() + s.getCost()) {
-                            frontier.remove(n);
-                            frontier.add(s);
+                        SearchNode frontier_node = frontier.stream().filter(x -> x.equals(child)).findFirst().get();
+                        if (frontier_node.getHeuristic() + frontier_node.getCost() > child.getHeuristic() + child.getCost()) {
+                            frontier.remove(frontier_node);
+                            frontier.add(child);
+                            break;
                         }
                     }
                 }
             }
         }
+        if (ArgParse.DEBUG) {
+            System.out.println("]");
+        }
         return false;
+    }
+
+    @Override
+    public double getHeuristic() {
+        return end_node.getHeuristic();
     }
 }
--- src/rushhour/ia/algo/recherche/DFS.java
+++ src/rushhour/ia/algo/recherche/DFS.java
@@ -1,46 +1,64 @@
 package rushhour.ia.algo.recherche;
 
 import rushhour.ia.framework.common.Action;
+import rushhour.ia.framework.common.ArgParse;
 import rushhour.ia.framework.common.State;
+import rushhour.ia.framework.recherche.HasHeuristic;
 import rushhour.ia.framework.recherche.SearchNode;
 import rushhour.ia.framework.recherche.SearchProblem;
 import rushhour.ia.framework.recherche.TreeSearch;
 
+import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.LinkedList;
 
-public class DFS extends TreeSearch {
-
-    /**
-     * Crée un algorithme de recherche
-     *
-     * @param p Le problème à résoudre
-     * @param s L'état initial
-     */
+public class DFS extends TreeSearch implements HasHeuristic {
     public DFS(SearchProblem p, State s) {
         super(p, s);
-        frontier = new LinkedList<>();
     }
 
     @Override
     public boolean solve() {
-        SearchNode node = SearchNode.makeRootSearchNode(intial_state);
-        frontier.add(node);
-        explored.add(node.getState());
-
+        end_node = SearchNode.makeRootSearchNode(intial_state);
+        intial_state = end_node.getState();
+        frontier = new LinkedList<>();
+        frontier.add(end_node);
+        explored = new HashSet<>();
+        if (ArgParse.DEBUG) {
+            System.out.print("[" + intial_state);
+        }
         while (!frontier.isEmpty()) {
-            node = frontier.poll();
-            if (problem.isGoalState(node.getState())) {
-                end_node = node;
-                return true;
+            end_node = frontier.remove();
+            intial_state = end_node.getState();
+            if (ArgParse.DEBUG) {
+                System.out.print(" + " + end_node.getAction() + "] -> [" + intial_state);
             }
-            explored.add(node.getState());
-            for (Action a : problem.getActions(node.getState())) {
-                SearchNode s = SearchNode.makeChildSearchNode(this.problem, node, a);
-                if (!explored.contains(s.getState()) && !frontier.contains(s)) {
-                    frontier.add(s);
+            if (problem.isGoalState(intial_state)) {
+                if (ArgParse.DEBUG) {
+                    System.out.println("]");
+                }
+                return true;
+            } else {
+                explored.add(intial_state);
+            }
+            for (Action a : problem.getActions(intial_state)) {
+                // depth first search
+                SearchNode child = SearchNode.makeChildSearchNode(problem, end_node, a);
+                State child_state = child.getState();
+                if (!frontier.contains(child) && !explored.contains(child_state)) {
+                    frontier.add(child);
                 }
             }
         }
+        if (ArgParse.DEBUG) {
+            System.out.println("]");
+        }
         return false;
+
+    }
+
+    @Override
+    public double getHeuristic() {
+        return 0;
     }
 }
